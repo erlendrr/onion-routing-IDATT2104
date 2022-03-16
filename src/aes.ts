8c440bb7d927cc8a59d8810860a6c7052eedb32a
Erlend Ryan <erlendryan@gmail.com>
2022-03-16 16:19:58 +0100

    onion

--- src/aes.ts
+++ src/aes.ts
@@ -3,21 +3,20 @@ import crypto from 'crypto'
 const ALGORITHM = 'aes-256-cbc'
 const ENCODING = 'hex'
 const IV_LENGTH = 16
-const KEY = '34743777217A25432A462D4A614E6452'
 
-export const encrypt = (data: string) => {
+export const encrypt = (data: string, key: string) => {
 	const iv = crypto.randomBytes(IV_LENGTH)
-	const cipher = crypto.createCipheriv(ALGORITHM, Buffer.from(KEY), iv)
+	const cipher = crypto.createCipheriv(ALGORITHM, Buffer.from(key), iv)
 	return Buffer.concat([cipher.update(data), cipher.final(), iv]).toString(
 		ENCODING
 	)
 }
 
-export const decrypt = (data: string) => {
+export const decrypt = (data: string, key: string) => {
 	const binaryData = Buffer.from(data, ENCODING)
 	const iv = binaryData.slice(-IV_LENGTH)
 	const encryptedData = binaryData.slice(0, binaryData.length - IV_LENGTH)
-	const decipher = crypto.createDecipheriv(ALGORITHM, Buffer.from(KEY), iv)
+	const decipher = crypto.createDecipheriv(ALGORITHM, Buffer.from(key), iv)
 
 	return Buffer.concat([
 		decipher.update(encryptedData),
--- src/client.ts
+++ src/client.ts
@@ -1,5 +1,67 @@
 import { encrypt, decrypt } from './aes'
 
-const data = {
-	name: 'John Doe',
+interface Address {
+	ip: string
+	port: number
 }
+
+interface Node {
+	address: Address
+	key: string
+}
+
+const nodes: Node[] = [
+	{
+		key: '34743777217A25432A462D4A614E6452',
+		address: {
+			ip: '127.0.0.1',
+			port: 8000,
+		},
+	},
+	{
+		key: '576D5A7134743777217A25432A462D4A',
+		address: {
+			ip: '127.0.0.1',
+			port: 8001,
+		},
+	},
+	{
+		key: '2A472D4A614E645267556B5870327335',
+		address: {
+			ip: '127.0.0.1',
+			port: 8002,
+		},
+	},
+]
+
+interface Block {
+	nextNodeAddress: Address
+	data: string
+}
+
+function createOnionBlock(data: string, nextNode: Node) {
+	return encrypt(
+		JSON.stringify({
+			nextNodeAddress: nextNode.address,
+			data: data,
+		}),
+		nextNode.key
+	)
+}
+
+const data = 'Hello world'
+const onionBlock2 = createOnionBlock(data, nodes[2])
+const onionBlock1 = createOnionBlock(onionBlock2, nodes[1])
+const onionBlock0 = createOnionBlock(onionBlock1, nodes[0])
+
+console.log(data)
+
+const block0: Block = JSON.parse(decrypt(onionBlock0, nodes[0].key))
+console.log(block0)
+
+const block1: Block = JSON.parse(decrypt(block0.data, nodes[1].key))
+console.log(block1)
+
+const block2 = JSON.parse(decrypt(block1.data, nodes[2].key))
+
+console.log(block2)
