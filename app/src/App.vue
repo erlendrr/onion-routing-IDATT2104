90feea6d12150545592347bbddb106af5056629a
Erlend Ryan <erlendryan@gmail.com>
2022-03-18 14:41:43 +0100

    vue app

---
+++ app/src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const htmlData = `
+	<div>
+		<h1>Hello</h1>
+		<p>This is a test</p>
+	</div>
+`
+</script>
+
+<template>
+	<article v-html="htmlData" class="prose"></article>
+</template>
