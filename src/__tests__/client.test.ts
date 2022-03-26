1884e40354824dad375785874756c47e873d9f2d
Erlend Ryan <erlendryan@gmail.com>
2022-03-26 07:23:03 +0100

    small tests

--- src/__tests__/client.test.ts
+++ src/__tests__/client.test.ts
@@ -1,3 +1,21 @@
-test('true = true', () => {
+it('sends a get request', () => {
+	const nodes = [
+		{ address: { ip: 'http://localhost', port: 8007 } },
+		{ address: { ip: 'http://localhost', port: 8002 } },
+		{ address: { ip: 'http://localhost', port: 8001 } },
+	]
+	const keys = [
+		'011815a3f5e9825b0394775fa37a8387',
+		'5c480c1a4bc4c4398e1280f03787b557',
+		'4d6ad1ad07f974de37fa388157abba14',
+	]
+	expect(true).toBe(true)
+})
+
+it('sends a post request', () => {
+	expect(true).toBe(true)
+})
+
+it('creates a node route', () => {
 	expect(true).toBe(true)
 })
--- src/__tests__/cryptoCustom.test.ts
+++ src/__tests__/cryptoCustom.test.ts
@@ -1,3 +1,17 @@
-test('true = true', () => {
+it('creates packets', () => {
+	const nodes = [
+		{ address: { ip: 'http://localhost', port: 8007 } },
+		{ address: { ip: 'http://localhost', port: 8002 } },
+		{ address: { ip: 'http://localhost', port: 8001 } },
+	]
+	const keys = [
+		'011815a3f5e9825b0394775fa37a8387',
+		'5c480c1a4bc4c4398e1280f03787b557',
+		'4d6ad1ad07f974de37fa388157abba14',
+	]
+	expect(true).toBe(true)
+})
+
+it('', () => {
 	expect(true).toBe(true)
 })
